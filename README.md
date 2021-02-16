3cfa42304f920d67303e417580f46559b16d8136
kotaoue <kotaoue@gmail.com>
2021-02-16 17:07:53 +0900

    add breakTime.

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ Timelc is a calculater for daily working time.
 
 ## Usage
 ```ShellSession
-$ go run main.go -s=09:45 -e=18:30 -m=30*4 -h=1
+$ go run main.go -s=09:45 -e=18:30 -m=30*4 -h=1 -b=60*1
 今日の作業
 予定 09:45-18:30
-業務時間 8時間45分
-作業時間 5時間45分
+業務時間 7時間45分
+作業時間 4時間45分
 会議時間 3時間00分
+休憩時間 1時間00分
 ```
--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 )
 
 type timeTable struct {
-	start   time.Time
-	end     time.Time
-	meeting time.Time
+	start     time.Time
+	end       time.Time
+	meeting   time.Time
+	breakTime time.Time
 }
 
 var tt timeTable
@@ -21,6 +22,7 @@ func init() {
 	e := flag.String("e", "18:30", "End time.")
 	m := flag.String("m", "0", "Meeting minute. e.g. 30*3")
 	h := flag.String("h", "0", "Meeting hour. e.g. 1*2")
+	b := flag.String("b", "60*1", "Break minute. e.g. 60*1")
 	flag.Parse()
 
 	tt.start, _ = time.Parse("15:04", *s)
@@ -28,17 +30,24 @@ func init() {
 
 	tt.meeting = calcNonWorkTime(tt.meeting, *m, time.Minute)
 	tt.meeting = calcNonWorkTime(tt.meeting, *h, time.Hour)
+
+	tt.breakTime = calcNonWorkTime(tt.breakTime, *b, time.Minute)
 }
 
 func main() {
 	fmt.Println("今日の作業")
 	fmt.Printf("予定 %s-%s\n", tt.start.Format("15:04"), tt.end.Format("15:04"))
-	total := tt.end.Sub(tt.start)
-	workStart := tt.start.Add((time.Duration(tt.meeting.Hour()) * time.Hour) + (time.Duration(tt.meeting.Minute()) * time.Minute))
+
+	md := (time.Duration(tt.meeting.Hour()) * time.Hour) + (time.Duration(tt.meeting.Minute()) * time.Minute)
+	bd := (time.Duration(tt.breakTime.Hour()) * time.Hour) + (time.Duration(tt.breakTime.Minute()) * time.Minute)
+
+	total := tt.end.Sub(tt.start.Add(bd))
+	workStart := tt.start.Add(md + bd)
 	work := tt.end.Sub(workStart)
 	fmt.Printf("業務時間 %d時間%02d分\n", (int(total.Hours()) % 24), (int(total.Minutes()) % 60))
 	fmt.Printf("作業時間 %d時間%02d分\n", (int(work.Hours()) % 24), (int(work.Minutes()) % 60))
 	fmt.Printf("会議時間 %d時間%02d分\n", tt.meeting.Hour(), tt.meeting.Minute())
+	fmt.Printf("休憩時間 %d時間%02d分\n", tt.breakTime.Hour(), tt.breakTime.Minute())
 }
 
 func calcNonWorkTime(t time.Time, s string, d time.Duration) time.Time {
